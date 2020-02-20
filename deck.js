dbb32ca76a42cf92640035c420900d25d5015004
Ally Jarjour <allyjarjour@gmail.com>
2020-02-19 18:01:16 -0700

    Hide win page on page load

--- deck.js
+++ deck.js
@@ -11,6 +11,6 @@ class Deck {
 
   }
   moveToMatched() {
-    
+
   }
 }
--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 var bCards = document.querySelector('.b-cards');
+var winScreen = document.querySelector('.win-screen');
+var mainScreen = document.querySelector('main');
+
+window.onload = function hideWinPage() {
+  winScreen.classList.add('hidden');
+}
+
+
+// sample event to add back in win page
+
+// bCards.addEventListener('click', switchScreens)
+//
+// function switchScreens() {
+//   winScreen.classList.remove('hidden');
+//   mainScreen.classList.add('hidden');
+// }
