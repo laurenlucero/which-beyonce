40e2735b54124b832739de8373b36e9a74c6286e
Ally Jarjour <allyjarjour@gmail.com>
2020-02-20 18:16:19 -0700

    Update check card function

--- deck.js
+++ deck.js
@@ -9,8 +9,10 @@ class Deck {
 
   }
   //method that checks if selected cards match
-  checkSelectedCards() {
-
+  checkSelectedCards(clickedCard) {
+    console.log(event.target.getAttribute('data-image'));
+    this.selectedCards = this.selectedCards.push(clickedCard);
+    // console.log(this.selectedCards);
   }
   // moves matched pairs to display in aside.
   moveToMatched() {
