72dff0b9435b09ad81998b2c1322de5df6559174
Ally Jarjour <allyjarjour@gmail.com>
2020-02-22 12:55:46 -0700

    Add shuffle functionality

--- deck.js
+++ deck.js
@@ -1,17 +1,29 @@
 class Deck {
-  constructor(cards) {
-    this.cards = cards;
+  constructor(cardObjects) {
+    this.cards = cardObjects;
     this.matchedCards = [];
     this.selectedCards = [];
   }
   //method that controls the card shuffle
   shuffle() {
+    // var randomPos = Math.floor(Math.random() * 10);
+    // for (var i = 0; i < this.cards.length; i++) {
+    // this.cards.style.order = randomPos;
+    var i = this.cards.length, j, temp;
+    while (--i > 0) {
+      j = Math.floor(Math.random() * (i + 1));
+      temp = this.cards[j];
+      this.cards[j] = this.cards[i];
+      this.cards[i] = temp;
+    }
+      console.log(this.cards);
+      return this.cards;
+    }
 
-  }
+
   //method that checks if selected cards match
   checkSelectedCards(clickedCard) {
     this.selectedCards.push(clickedCard);
-    // for (var i = 0; i > this.selectedCards.length; i++)
     if (this.selectedCards[0] === this.selectedCards[1]) {
       this.matchedCards.push(this.selectedCards[0], this.selectedCards[1]);
       this.selectedCards.pop();
@@ -19,6 +31,7 @@ class Deck {
     } else if (this.selectedCards.length === 2 && this.selectedCards[0] !== this.selectedCards[1]) {
       this.selectedCards.pop();
       this.selectedCards.pop();
+      // where/when we need to flip cards back over
     }
     console.log(this.matchedCards);
     console.log(this.selectedCards);
