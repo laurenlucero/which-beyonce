fab91d0082db45cb228e3a6cb4fe8c3ebd34f909
“Lauren <lucerolaurenm@gmail.com>
2020-02-23 13:47:48 -0700

    Add unflip functionality

--- cards.js
+++ cards.js
@@ -3,15 +3,12 @@ class Card {
     this.matchInfo = matchInfo;
     this.matched = false;
     this.altInfo = altInfo;
-    
   }
-
   match() {
-    // We will want to match our values
     for (var i = 0; i < deck.matchedCards.length; i++) {
-    if (this.matchInfo === deck.matchedCards[i]) {
+    if (deck.matchedCards[i].includes(this.matchInfo)) {
       this.matched = true;
+    }
     }
   }
   }
-}
--- deck.js
+++ deck.js
@@ -4,11 +4,9 @@ class Deck {
     this.matchedCards = [];
     this.selectedCards = [];
   }
+
   //method that controls the card shuffle
   shuffle() {
-    // var randomPos = Math.floor(Math.random() * 10);
-    // for (var i = 0; i < this.cards.length; i++) {
-    // this.cards.style.order = randomPos;
     var i = this.cards.length, j, temp;
     while (--i > 0) {
       j = Math.floor(Math.random() * (i + 1));
@@ -16,26 +14,22 @@ class Deck {
       this.cards[j] = this.cards[i];
       this.cards[i] = temp;
     }
-      console.log(this.cards);
       return this.cards;
     }
-
 
   //method that checks if selected cards match
   checkSelectedCards(clickedCard) {
     this.selectedCards.push(clickedCard);
     if (this.selectedCards[0] === this.selectedCards[1]) {
       this.matchedCards.push(this.selectedCards[0], this.selectedCards[1]);
-      this.selectedCards.pop();
-      this.selectedCards.pop();
-    } else if (this.selectedCards.length === 2 && this.selectedCards[0] !== this.selectedCards[1]) {
-      this.selectedCards.pop();
-      this.selectedCards.pop();
-      // where/when we need to flip cards back over
+      // this.selectedCards.pop();
+      // this.selectedCards.pop();
     }
-    console.log(this.matchedCards);
     console.log(this.selectedCards);
+    console.log(this.matchedCards);
+    console.log(cardObjects);
   }
+
   // moves matched pairs to display in aside.
   moveToMatched() {
 
