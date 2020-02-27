5381e523ec0d8ffbbab9c189503b98747bb5344b
“Lauren <lucerolaurenm@gmail.com>
2020-02-27 13:09:11 -0700

    Remove card ID and change play again button

--- cards.js
+++ cards.js
@@ -1,9 +1,8 @@
 class Card {
-  constructor(matchInfo, altInfo, cardId) {
+  constructor(matchInfo, altInfo) {
     this.matchInfo = matchInfo;
     this.matched = false;
     this.altInfo = altInfo;
-    this.cardId = cardId;
   }
   match() {
     for (var i = 0; i < deck.matchedCards.length; i++) {
